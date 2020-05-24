418f9e5e1b05c7463bb4279508ed74c20637005a
YuNa Gal <yunagal@YuNas-MacBook-Pro.local>
2020-05-24 16:56:30 +0900

    public initial

---
+++ saju/public/fireinit.js
@@ -0,0 +1,19 @@
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+// Required for side-effects
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAojvj6UzMVGc2cNRqpv2wA0fkSjEVB0tU",
+//   authDomain: "funsaju-52333.firebaseapp.com",
+//   databaseURL: "https://funsaju-52333.firebaseio.com",
+//   projectId: "funsaju-52333",
+//   storageBucket: "funsaju-52333.appspot.com",
+//   messagingSenderId: "1095373287542",
+//   appId: "1:1095373287542:web:29c7ba9db8a37467f8af57",
+//   measurementId: "G-VTQ1V1RXTH"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 4={7:"8",9:"0-2.a.3",b:"c://0-2.d.3",e:"0-2",f:"0-2.g.3",h:"5",i:"1:5:j:k",l:"m-n"};o.p(4);',26,26,'funsaju||52333|com|firebaseConfig|1095373287542|const|apiKey|AIzaSyAojvj6UzMVGc2cNRqpv2wA0fkSjEVB0tU|authDomain|firebaseapp|databaseURL|https|firebaseio|projectId|storageBucket|appspot|messagingSenderId|appId|web|29c7ba9db8a37467f8af57|measurementId|G|VTQ1V1RXTH|firebase|initializeApp'.split('|'),0,{}))
